30c8280b241029cf04ecd9dba0a6a7c3f67c24e2
E-bot-rgb <zebastianericsson@gmail.com>
2025-12-14 20:05:52 +0100

    Use of test data

---
+++ DB_Scripts/02_insert_test_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO Artists (Name, Country) VALUES
+('Pink Floyd', 'UK'),
+('Taylor Swift', 'USA');
+
+INSERT INTO Albums (Title, ReleaseYear, ArtistId) VALUES
+('The Dark Side of the Moon', 1973, 1),
+('1989', 2014, 2);
+
+INSERT INTO Tracks (Title, Duration, AlbumId) VALUES
+('Time', 412, 1),
+('Style', 231, 2);
